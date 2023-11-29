6f98d13b98bea0d04520f0ff64ca44ca30b7225d
David-K <krestan.david@gmail.com>
2023-11-29 22:14:53 +0100

    Exit button funguje

--- src/main/java/com/example/graphicsgui/HelloController.java
+++ src/main/java/com/example/graphicsgui/HelloController.java
@@ -1,7 +1,17 @@
 package com.example.graphicsgui;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+
+import java.io.IOException;
+
 
 public class HelloController {
     @FXML
@@ -11,4 +21,15 @@ public class HelloController {
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+
+    @FXML
+    private void exitButtonAction() {
+        Platform.exit();
+    }
+    @FXML
+    private void aboutButtonAction(){
+
+    }
+
+
 }
