666c205d791a645ee44aa9a4663fe0fd9267bd49
David-K <krestan.david@gmail.com>
2023-11-29 19:14:26 +0100

    size

--- src/main/java/com/example/graphicsgui/HelloApplication.java
+++ src/main/java/com/example/graphicsgui/HelloApplication.java
@@ -11,7 +11,9 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 500, 800);
+        stage.setMinHeight(500);
+        stage.setMinWidth(800);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
