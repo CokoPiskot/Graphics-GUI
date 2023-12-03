12fd279f0510532ef88a18b3e619ea553527f4b7
David-K <krestan.david@gmail.com>
2023-12-03 19:34:55 +0100

    default size fix

--- src/main/java/com/example/graphicsgui/HelloApplication.java
+++ src/main/java/com/example/graphicsgui/HelloApplication.java
@@ -21,7 +21,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
 
-        Scene scene = new Scene(fxmlLoader.load(), 500, 800);
+        Scene scene = new Scene(fxmlLoader.load(), 800, 500);
 
         HelloController controller = fxmlLoader.getController();
         //controller.resizableTextArea();
