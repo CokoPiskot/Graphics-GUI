9fa4c9dd5df4a5e220ed0f1d42fd939f4349fa42
David-K <krestan.david@gmail.com>
2023-12-03 15:27:42 +0100

    predelany text prompt ale jeste to neni idealní

--- src/main/java/com/example/graphicsgui/HelloApplication.java
+++ src/main/java/com/example/graphicsgui/HelloApplication.java
@@ -1,21 +1,35 @@
 package com.example.graphicsgui;
-
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-
 import java.io.IOException;
+import java.util.Objects;
+
+import javafx.scene.Parent;
+
+
 
 public class HelloApplication extends Application {
+
+    @FXML
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+
         Scene scene = new Scene(fxmlLoader.load(), 500, 800);
+
+        HelloController controller = fxmlLoader.getController();
+        //controller.resizableTextArea();
+
         stage.setMinHeight(500);
         stage.setMinWidth(800);
         stage.setTitle("Hello!");
-        int x= 56;
+
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/example/graphicsgui/HelloController.java
+++ src/main/java/com/example/graphicsgui/HelloController.java
@@ -5,30 +5,42 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 
 import java.io.IOException;
+import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
+
+
 
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private AnchorPane pane;
+
+    @FXML
+    private TextArea textArea;
 
     @FXML
     private void exitButtonAction() {
         Platform.exit();
     }
+
+    @FXML
+    public void resizableTextArea() {
+        pane.getChildren().add(textArea);
+    }
+
     @FXML
     private void aboutButtonAction(){
-
     }
 
 
