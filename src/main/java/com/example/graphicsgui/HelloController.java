be7cb0b173f6a5bd969d07526c31bd8f658c0cbf
RedeemHub <89012989+RedeemHub@users.noreply.github.com>
2023-12-04 20:26:25 +0100

    Added Select Image Function

--- src/main/java/com/example/graphicsgui/HelloController.java
+++ src/main/java/com/example/graphicsgui/HelloController.java
@@ -4,16 +4,51 @@ import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 
 public class HelloController {
+
+
 
     @FXML
     private void exitButtonAction() {
         Platform.exit();
     }
+
+    @FXML
+    private ImageView imageView;
+
+    @FXML
+    private MenuItem selectImageButtonMenu;
+
+    @FXML
+    private Button selectImageButton;
+
+    @FXML
+    private void selectImage() {
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle("Select Image File");
+            File selectedFile = fileChooser.showOpenDialog(null);
+
+            if (selectedFile != null) {
+                try {
+                    Image image = new Image(new FileInputStream(selectedFile));
+                    imageView.setImage(image);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
 
     @FXML
     private void aboutButtonAction() throws IOException{
